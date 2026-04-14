9e5cb7b85477d023dfbe07677f82e2fbae83acd7
shantothomas501 <shantothomas501@gmail.com>
2026-04-14 08:51:44 +0530

    seventh commit

--- app.js
+++ app.js
@@ -46,20 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Auto-play on first interaction
-    const startMusic = () => {
-        if (!isPlaying) {
-            bgMusic.play().then(() => {
-                isPlaying = true;
-                musicTrigger.classList.add('playing');
-                musicTrigger.innerHTML = '<i class="fas fa-pause"></i><div class="music-tooltip">Pause Music</div>';
-            }).catch(() => {});
-            document.removeEventListener('click', startMusic);
-            document.removeEventListener('touchstart', startMusic);
-        }
-    };
-    document.addEventListener('click', startMusic);
-    document.addEventListener('touchstart', startMusic);
+
 
     // Countdown Timer Logic
     const weddingDate = new Date("April 30, 2026 10:30:00").getTime();
